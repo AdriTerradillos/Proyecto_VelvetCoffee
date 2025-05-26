af01bbe37c65c6b7008ef02f30d82b000147aac2
AdriTerradillos <adrian.terradillos@students.gammatech.school>
2025-05-26 14:27:50 +0200

    Nueva Actualización: agregando el proyecto a partir de --> http://start.spring.io/

---
+++ velvetCoffee/src/main/java/com/proyecto/velvetCoffee/VelvetCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.velvetCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelvetCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelvetCoffeeApplication.class, args);
+	}
+
+}
---
+++ velvetCoffee/src/test/java/com/proyecto/velvetCoffee/VelvetCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.velvetCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelvetCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
